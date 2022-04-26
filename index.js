5662dc008043e33f615f2a7fd4b2971e5286a999
19520923 <19520923@gm.uit.edu.vn>
2022-04-26 22:43:51 +0700

    limit express

--- index.js
+++ index.js
@@ -36,8 +36,9 @@ mongoose
     })
     .catch((err) => console.log(err))
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
-app.use(express.json())
 app.use(helmet())
 app.use(morgan("common"))
 app.use(cors())
