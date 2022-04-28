0c72b75e685e69ef223fe6a6e097e2a3a589114a
19520923 <19520923@gm.uit.edu.vn>
2022-04-28 18:24:49 +0700

    notification

--- utils/CreateNotification.js
+++ utils/CreateNotification.js
@@ -14,5 +14,5 @@ module.exports = async ({receiver, content, destination = '', type = 'SYSTEM', a
 
     const savedNotif = await notification.save()
 
-    return FilterNotification(savedNotif)
+    return savedNotif
 }
