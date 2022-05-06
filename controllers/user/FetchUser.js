5c9b5ce5762faa1d290a382c3e7efda4f7235ef5
19520923 <19520923@gm.uit.edu.vn>
2022-05-06 23:36:16 +0700

    fix user

--- controllers/user/FetchUser.js
+++ controllers/user/FetchUser.js
@@ -6,10 +6,9 @@ const FilterNotification = require('../../utils/FilterNotification')
 exports.fetchUserById = async (req, res) => {
     try {
         const user = await User.findById(req.params.user_id).populate('follower').populate('following')
-        const userData = FilterUserData(user)
 
         return res.status(200).json({
-            user: userData
+            user: user
         })
     } catch (err) {
         console.log(err)
@@ -31,12 +30,8 @@ exports.searchUsers = async (req, res) => {
         }).populate('follower')
             .populate('following')
 
-        const usersData = users.map(user =>
-            FilterUserData(user)
-        )
-
         return res.status(200).json({
-            users: usersData
+            users: users
         })
     } catch (err) {
         console.log(err)
@@ -56,48 +51,10 @@ exports.me = async (req, res) => {
             })
         }
 
-        const userData = FilterUserData(user)
-
-        const follower = user.follower.map(f => {
-            return {
-                id: f.id,
-                first_name: f.first_name,
-                last_name: f.last_name,
-                username: f.username,
-                email: f.email,
-                is_current: f.is_current,
-                avatar_url: f.avatar_url,
-                cover_url: f.cover_url,
-                about: f.about,
-                last_login: f.last_login,
-                created_at: f.created_at,
-            }
-
-        })
-
-        const following = user.following.map(f => {
-            return {
-                id: f.id,
-                first_name: f.first_name,
-                last_name: f.last_name,
-                username: f.username,
-                email: f.email,
-                is_current: f.is_current,
-                avatar_url: f.avatar_url,
-                cover_url: f.cover_url,
-                about: f.about,
-                last_login: f.last_login,
-                created_at: f.created_at,
-            }
-        })
-
-        userData.follower = follower
-        userData.following = following
-
         const notifications = await Notification.find({ receiver: req.userId }).sort({ create_at: -1 }).populate('author')
 
         return res.status(200).json({
-            user: userData,
+            user: user,
             notifications: notifications
         })
     } catch (err) {
