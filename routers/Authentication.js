486578a7c9a0b723a1744f34d6730cbd2f12b89f
19520923 <19520923@gm.uit.edu.vn>
2022-05-12 09:09:36 +0700

    update profile

--- routers/Authentication.js
+++ routers/Authentication.js
@@ -1,17 +1,19 @@
 const router = require('express').Router()
-const {RegisterUser, VerifyEmail} = require('../controllers/authentication/Signup')
+const { RegisterUser, VerifyEmail } = require('../controllers/authentication/Signup')
 const Login = require('../controllers/authentication/Login')
 const Logout = require('../controllers/authentication/Logout')
 
 const authRequired = require('../middleware/AuthMiddleware')
 const { changePassword, resetPassword } = require('../controllers/authentication/ResetPassword')
+const { updateProfile } = require('../controllers/user/UserActions')
 
 router.post('/signup', RegisterUser)
 router.get('/verify/:token', VerifyEmail)
 
 router.post('/login', Login)
-router.post('/logout',authRequired, Logout)
+router.post('/logout', authRequired, Logout)
 router.post('/change-password', authRequired, changePassword)
 router.post('/reset-password', resetPassword)
+router.post('/update-profile', authRequired, updateProfile)
 
 module.exports = router
